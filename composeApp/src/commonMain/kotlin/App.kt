0f388900869e3edbf60e1074117ed21d50e9bf2d
CzinkeM <mczinke-dev@proton.me>
2024-09-23 09:09:30 +0200

    Removed di initialization from common sourcecode.

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,9 +1,7 @@
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
-import di.appModule
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.koin.core.context.startKoin
 import presentation.MainScreen
 import theme.colorScheme
 import theme.shapes
@@ -11,17 +9,10 @@ import theme.shapes
 @Composable
 @Preview
 fun App() {
-    initKoin()
     MaterialTheme(
         shapes = shapes,
         colors = colorScheme
     ) {
         Navigator(MainScreen())
-    }
-}
-
-private fun initKoin() {
-    startKoin {
-        modules(appModule)
     }
 }
